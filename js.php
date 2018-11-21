143812f499a8b707a4dd752d72241c51177c37cc
Al Mamun <asifulmamun@gmail.com>
2018-11-21 17:38:10 +0600

    .

--- js.php
+++ js.php
@@ -4,6 +4,3 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <!-- theme js -->
 <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/theme.js"></script>
-
-
-
