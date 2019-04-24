4e7855c729db3ffb6378d896d5069eaf57126f44
asifulmamun <asifulmamun@gmail.com>
2019-04-24 17:47:33 +0600

    css and footer php added london address for mahfuz islam

--- footer.php
+++ footer.php
@@ -13,13 +13,14 @@
 	</div><!-- #content -->
 
 	<section id="footer">
+		<address>6 Duckett Street<br>London E1 4LU<br>United Kingdom<br>+447578088999</address>
 		<footer id="colophon" class="site-footer">
 			<div class="site-info">
 				<span class="sep">&copy Non-Resident Bangladesh Rights Movement 2018</span>
 			</div><!-- .site-info -->
 			<div class="footer_social_menu">
 				<span>
-					<a href="https://facebook.com/asifulmamun">
+					<a href="https://facebook.com/nrbrightsmovement">
 						<i class="fab fa-facebook-square"></i>
 					</a>
 				</span>
@@ -34,23 +35,15 @@
 					</a>
 				</span>
 			</div>
+			<i><a style="font-size:10px;color:#0006;font-weight:normal;" title="To know Developer of site click here..!" href="https://facebook.com/asifulmamun">Developer @asifulmamun</a></i>
 		</footer><!-- #colophon -->
-	</section> <!-- #footer -->
 
+	</section> <!-- #footer -->
+	
 </div><!-- #page -->
 <!-- Scroll Top Button -->
 <button onclick="scrollTopFunction()" id="scrollTop" title="Go to top"><i class="fa fa-arrow-up"></i></button> 
-
-
-<div id="window"></div>
-
-<script type="text/javascript">
-	
-
-</script>
-
 <?php wp_footer(); ?>
-
 <!-- js -->
 <?php require_once('js.php'); ?>
 </body>
